df1a284bbdc0ce0c2154f8b69c6659edc21977c8
Eduardo Rui <eduardorui1617@gmail.com>
2023-07-01 22:15:20 -0300

    add dominó

---
+++ OPERADOR - MODULO/Dominó - OBI 2019 F1PJ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int N;
+    int calculo;
+    cin >> N;
+    
+    calculo = ((N+1) * (N+2)) /2;
+    
+    cout << calculo << endl;
+}
